9379feffdaab9f50dd468d45959043a0446bb749
Bensen Fan <bensenfan88@gmail.com>
2017-04-16 19:10:13 -0700

    changed kayla's title, make core member image non-clicable

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,7 +9,7 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About Democracy@Work - Los Angeles';
 
 	locals.organisers = [
-		{ name: 'Kayla Al-Shamma-Jones', image: '/images/organisers/kayla_jones.jpg', twitter: 'twalve', title: 'Research/Development', profile: '/member/sharkie' },
+		{ name: 'Kayla Al-Shamma-Jones', image: '/images/organisers/kayla_jones.jpg', twitter: 'twalve', title: 'Research/Social Media', profile: '/member/sharkie' },
 		{ name: 'Elizabeth Phillips', image: '/images/organisers/liz_phillips.jpg', twitter: 'jedwatson', title: 'Content Production', profile: '/member/jed-watson' },
 		{ name: 'Andrea Iannone', image: '/images/organisers/andrea_iannone.jpg', twitter: 'geekyjohn', title: 'Fundrasing/Outreach', profile: '/member/john-van-der-loo' }
 	]
