0b81c73ffba1e75024fb2b7c578f5c34f97e7d96
Bensen Fan <bensenfan88@gmail.com>
2017-04-16 19:26:37 -0700

    trying to fix bug display meet up time

--- models/Meetup.js
+++ models/Meetup.js
@@ -19,8 +19,8 @@ Meetup.add({
 
 	state: { type: Types.Select, options: 'draft, scheduled, active, past', noedit: true },
 
-	startDate: { type: Types.Datetime, required: true, initial: true, index: true, width: 'short', note: 'e.g. 2014-07-15 / 6:00pm' },
-	endDate: { type: Types.Datetime, required: true, initial: true, index: true, width: 'short', note: 'e.g. 2014-07-15 / 9:00pm' },
+	startDate: { type: Types.Datetime, required: true, initial: true, index: true, width: 'short', note: 'e.g. 2014-07-15 / 6:00:00 pm' },
+	endDate: { type: Types.Datetime, required: true, initial: true, index: true, width: 'short', note: 'e.g. 2014-07-15 / 9:00:00 pm' },
 
 	place: { type: String, required: false, initial: true, width: 'medium', default: '1260 18th Street, Santa Monica, CA 90404', note: '' },
 	map: { type: String, required: false, initial: true, width: 'medium', default: '1260 18th Street, Santa Monica, CA 90404', note: '' },
